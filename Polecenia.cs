5d3a0e169740a7fae6942455771806720622ef5f
Daniel Pawłowski <UL0234272@edu.uni.lodz.pl>
2021-05-12 15:40:38 +0200

    Add project files.

---
+++ Polecenia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppPolecanie
+{
+    public class Polecenia
+    {
+        public string Polecenie { get; set; }
+
+        public List<string> Argumenty { get; set; }
+
+        public Dictionary<string, string> Opcje { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AppPolecanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string TrescPolecenia = "Def string tryb -domyslny debug -elementy 1,2,3,4 -resetuj -separator";
+
+            string[] subs = TrescPolecenia.Split(' ');
+
+           //foreach (var sub in subs)
+           //{
+                //Console.WriteLine($"Substring: {sub}");
+
+                var Tpol = TrescPolecenia.Substring(0, TrescPolecenia.IndexOf(" "));
+
+                //var Targ = TrescPolecenia.Substring(3, TrescPolecenia.IndexOf(" -"));
+                int T1Targ = TrescPolecenia.IndexOf(" ") + " ".Length;
+                int T2Targ = TrescPolecenia.IndexOf(" -");
+                var Targ = TrescPolecenia.Substring(T1Targ, T2Targ - T1Targ);
+                
+                int T1Topcje = TrescPolecenia.IndexOf(" -") + " ".Length;
+                int T2Topcje = TrescPolecenia.LastIndexOf("");
+                var Topcje = TrescPolecenia.Substring(T1Topcje, T2Topcje - T1Topcje);
+                //var Tmiedzy = Tpol - Targ;
+                //var Topc
+
+                Console.WriteLine("Polecenie= " + Tpol);
+                Console.WriteLine("Argumenty= " + Targ);
+                Console.WriteLine("Opcje= "+ Topcje);
+           // }
+
+        }
+    }
+}
