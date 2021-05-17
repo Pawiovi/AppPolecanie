471bc09cb1c22a8c8db166829fe198c4627b4fd7
Daniel Pawłowski <UL0234272@edu.uni.lodz.pl>
2021-05-17 14:17:29 +0200

    test 17.05.2021 14:17

--- Program.cs
+++ Program.cs
@@ -77,6 +77,7 @@ namespace AppPolecanie
                     //Console.WriteLine(ch);
                 }
 
+                //TEST
                 //Console.WriteLine(polecenie.Literea);
                 if (polecenie.Literea == " ")
                 {
